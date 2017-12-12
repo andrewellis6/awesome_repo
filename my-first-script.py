2e015eb80bf4c6235b24be381eb20018c9f4e773
andrewellis6 <34469845+andrewellis6@users.noreply.github.com>
2017-12-12 00:19:29 -0700

    Add files via upload

---
+++ my-first-script.py
@@ -0,0 +1,9 @@
+def welcome_message(name):
+#Prints out a personalised welcome message
+    return "Welcome to this Python script, " + name + "!"
+
+#Call the welcome message function with the input "Udacity Student"
+#and print the output
+print(welcome_message("Andrew Ellis"))
+print("I hope you are doing well today!")
+#the previous line is Andrew fooling around on the program.
